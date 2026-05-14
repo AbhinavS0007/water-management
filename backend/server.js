aecf4db2658e0b4c2eeae878320817d2a6626816
AbhinavS0007 <abhinavsachan635@gmail.com>
2026-05-14 22:12:41 +0530

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: [ process.env.FRONTEND_URL],
+  origin: [ process.env.FRONTEND_URL, "https://water-management-o141.vercel.app"],
   credentials: true
 }));
 
